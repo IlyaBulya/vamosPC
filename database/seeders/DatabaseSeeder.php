3396ecd846aab747476f6df40ca90c3c5a4f7598
NikitaT <niknik06042005@gmail.com>
2026-03-12 17:16:50 +0700

    Added configuration seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class ,
             ProductSeeder::class ,
+            ConfigurationSeeder::class ,
         ]);
 
         DB::table('users')->insert([
