cab096d1b23eeffc3753404ca9ebbfdadd444f48
NikitaT <niknik06042005@gmail.com>
2026-03-17 14:57:42 +0700

    Removed the seeded admins

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,40 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategorySeeder::class ,
-            ProductSeeder::class ,
-            ConfigurationSeeder::class ,
+            CategorySeeder::class,
+            ProductSeeder::class,
+            ConfigurationSeeder::class,
         ]);
-
-        $admins = [
-            [
-                'name' => 'Nikita Tsekhomskiy',
-                'email' => 'ntsekhomskiy@gmail.com',
-                'password' => '$2y$12$fQ5xW8m3LSablfk2v/qKCeEuQ8B.ZEfY6ecvauxG1LaHo4AX5U6fi',
-            ],
-            [
-                'name' => 'Ilya Bulya',
-                'email' => 'ilyade3004@gmail.com',
-                'password' => '$2y$12$Yfw65GWrP7./Y/KKviH6OexRXd0R1i0LEDese2viYW9BuqUAGCJay',
-            ],
-        ];
-
-        foreach ($admins as $adminData) {
-            $admin = User::query()->firstOrNew([
-                'email' => $adminData['email'],
-            ]);
-
-            $admin->forceFill([
-                'name' => $adminData['name'],
-                'email_verified_at' => $admin->email_verified_at ?? now(),
-                'is_admin' => true,
-            ]);
-
-            if (! $admin->exists) {
-                $admin->password = $adminData['password'];
-            }
-
-            $admin->save();
-        }
     }
 }
